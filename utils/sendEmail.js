215bd1ee0a1f99ccfd68e14fc1e19057362cd0c4
Nguyễn Tấn Tú <nguyentantu230704@gmail.com>
2026-03-29 11:23:39 +0700

    sửa port gửi mail từ 587 thành 465

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,23 +2,26 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     // 1. Tạo transporter (Cấu hình server mail)
+    // Nâng cấp: Tự động đổi secure = true nếu dùng port 465
+    const smptPort = process.env.SMTP_PORT || 587;
+    const isSecure = smptPort == 465;
+
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false, // true cho port 465, false cho các port khác
+        port: smptPort,
+        secure: isSecure, // Thông minh tự đổi thành true nếu là 465
         auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD,
         },
     });
 
-    // 2. Định nghĩa nội dung mail
     const message = {
         from: `${process.env.FROM_NAME} <${process.env.SMTP_EMAIL}>`,
         to: options.email,
         subject: options.subject,
-        text: options.message, // Fallback text
-        html: options.html // HTML template đẹp mắt
+        text: options.message,
+        html: options.html,
     };
 
     // 3. Gửi mail
